bbb6de321680f79df472a24fd4acd3be14fa9a45
nokuudinge <727573@student.inholland.nl>
2025-03-13 02:07:06 +0100

    Created Model for Students

---
+++ someren application/Models/Students.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace someren_application.Models
+{
+    public class Students : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
