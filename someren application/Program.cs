5428504b198c7136c16817d1628583f515562a78
amazingnl <726138@student.inholland.nl>
2025-03-13 20:37:20 +0100

    I wrote the room code, add the IRoomRepository, DbRoomRepository, Create, Edit and Index view of room, RoomController and Room.cs

--- someren application/Program.cs
+++ someren application/Program.cs
@@ -1,3 +1,4 @@
+using someren_application.Repositories;
 namespace someren_application
 {
     public class Program
@@ -7,7 +8,10 @@ namespace someren_application
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddSingleton<IRoomRepository, DbRoomRepository>();
             builder.Services.AddControllersWithViews();
+
 
             var app = builder.Build();
 
---
+++ someren application/Repositories/IRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using someren_application.Models;
+
+namespace someren_application.Repositories
+{
+    public interface IRoomRepository 
+    {
+        List<Room> GetAll();
+        Room? GetById(int userId);
+        void Add(Room room);
+        void Update(Room room);
+        void Delete(Room room);
+    }
+}
